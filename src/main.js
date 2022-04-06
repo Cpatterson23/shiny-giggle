55e6c79e03da3c47ac15d814a43d52a8b86c2503
unknown <aacpatterson@yahoo.com>
2022-04-05 20:15:12 -0500

    learning strings.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const myName = "cedrick";
+
+
+
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+console.log(myName.replace("ck","c"));
+console.log(myName.replace("ce","a" ));
+console.log("my name is " + myName );
+console.log(`${myName} P`);
+
